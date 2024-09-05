f4a1cb6052be39f72bfa5d74865af7b21b2ee52b
felirx <felirx@gmail.com>
2024-09-05 23:55:45 +0300

    tsserver name deprecation

--- init.lua
+++ init.lua
@@ -589,7 +589,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        vtsls = {},
         --
 
         lua_ls = {
